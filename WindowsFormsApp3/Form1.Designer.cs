fcf282d448c7803d9fed2401777ebf1168bdd455
sanglyb <sanglyb@mail.ru>
2023-02-17 17:24:07 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 399);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Продолжить работу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 399);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 80);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Завершить сессию";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(17, 225);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(625, 165);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(632, 207);
+            this.label1.TabIndex = 4;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 495);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ошибка загрузки профиля";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Security.Cryptography;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Xml.Linq;
+using System.Management.Automation;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace WindowsFormsApp3
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int ExitWindowsEx(int uFlags, int dwReason);
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Close();
+        }
+        
+        private void send_message(string userName, string sid, string message, string time)
+        {
+            int port = Convert.ToInt32(ConfigurationSettings.AppSettings["port"]);
+            string recipients= ConfigurationSettings.AppSettings["recipients"];
+            string sender= ConfigurationSettings.AppSettings["sender"];
+            string mailserver= ConfigurationSettings.AppSettings["mailserver"];
+        
+
+
+            var smtpClient = new System.Net.Mail.SmtpClient(mailserver)
+            {
+                Port = port,
+            };
+            string recipient = recipients;
+            string subject = "TSFarm profile problem";
+            string body = "Произошла ошибка подключения диска профиля" + 
+                "\nИмя пользователя " + userName +
+                " " + sid + "\nНа сервере - " + Environment.MachineName.ToString() + "\n"
+                + message
+                + "\nВремя события - " + time;
+            string email = sender;
+            try
+            {
+                smtpClient.Send(email, recipient, subject, body);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private string powershellRun(string sid)
+        {
+            PowerShell powerShellInstance = PowerShell.Create();
+            powerShellInstance.AddScript("Get-Disk | select location | where {$_.Location -notmatch \"Integrated\" -AND $_.location -notmatch \"SCSI0\"}");
+            Collection<PSObject> psOutput = powerShellInstance.Invoke();
+            bool profileFound = false;
+            string result = null;
+            foreach (PSObject outputItem in psOutput)
+            {
+                if (outputItem != null)
+                {
+                    string disklocation = outputItem.Properties["location"].Value.ToString();
+                    if (disklocation.Contains(sid))
+                    {
+                        profileFound = true;
+                        if (disklocation.IndexOf(sid + "_ro.vhdx", 0, StringComparison.OrdinalIgnoreCase) != -1 ||
+                    disklocation.IndexOf("c:\\windows\\temp", 0, StringComparison.OrdinalIgnoreCase) != -1)
+                        {
+                            result = disklocation;
+                        }
+                        else
+                        {
+                            result = null;
+                        }
+                        break;
+                    }
+                }
+            }
+            if (!profileFound)
+            {
+                result = "Профиль не найден";
+            }
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExitWindowsEx(0, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "Сведения для администраторов: \n";
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string sid = System.Security.Principal.WindowsIdentity.GetCurrent().User.ToString();
+            string logType = "Microsoft-FSLogix-Apps/Operational";
+            string query = "*[System[(EventID=51) and Security[@UserID='" + sid + "'] " +
+                "and TimeCreated[timediff(@SystemTime) <= 120000]]]";
+            string query1 = "*[System[(EventID=26) " +
+                "and TimeCreated[timediff(@SystemTime) <= 120000]]]";
+            string time = null;
+            string message = null;
+
+            try
+            {
+                var elQuery = new EventLogQuery(logType, PathType.LogName, query);
+                var elReader = new EventLogReader(elQuery);
+                for (EventRecord eventInstance = elReader.ReadEvent(); eventInstance != null; eventInstance = elReader.ReadEvent())
+                {
+                    if (eventInstance.FormatDescription() != null) {
+                        message = eventInstance.FormatDescription();
+                        time = eventInstance.TimeCreated.ToString();
+                        richTextBox2.Text = "Сведения для администраторов:" +
+                            "\nИмя пользователя: " + userName +
+                            "\nsid: " + sid +
+                            "\nСообщение: " + message +
+                            "\nИмя компьютера: " + Environment.MachineName.ToString() +
+                            "\nВремя:" + time;
+
+                        send_message(userName, sid, message, time);
+                        break;
+                    }
+                }
+                if (message == null)
+                {
+                    message = powershellRun(sid);
+                    if (message != null)
+                    {
+                        try
+                        {
+                            var elQuery1 = new EventLogQuery(logType, PathType.LogName, query1);
+                            var elReader1 = new EventLogReader(elQuery1);
+                            for (EventRecord eventInstance = elReader1.ReadEvent();
+                                eventInstance != null; eventInstance = elReader1.ReadEvent())
+                            {
+                                if (eventInstance.FormatDescription() != null)
+                                {
+                                    if (eventInstance.FormatDescription().IndexOf(sid, 0, StringComparison.OrdinalIgnoreCase) != -1)
+                                    {
+                                        message += "\n" + eventInstance.FormatDescription();
+                                        time = eventInstance.TimeCreated.ToString();
+                                        break;
+                                    }
+
+                                }
+                            }
+                        }
+                        catch { }
+
+                        if (time == null)
+                        {
+                            time = DateTime.Now.ToString();
+                        }
+
+                        richTextBox2.Text = "Сведения для администраторов:" +
+                            "\nИмя пользователя: " + userName +
+                            "\nsid: " + sid +
+                            "\nСообщение: " + message +
+                            "\nИмя компьютера: " + Environment.MachineName.ToString() +
+                            "\nВремя:" + time;
+                        send_message(userName, sid, message, time);
+                    }
+                    else
+                    {
+                        this.Close();
+                    }
+                }
+            } 
+            catch
+            {
+                message = powershellRun(sid);
+                if (message != null)
+                {
+                    try
+                    {
+                        var elQuery1 = new EventLogQuery(logType, PathType.LogName, query1);
+                        var elReader1 = new EventLogReader(elQuery1);
+                        for (EventRecord eventInstance = elReader1.ReadEvent();
+                            eventInstance != null; eventInstance = elReader1.ReadEvent())
+                        {
+                            if (eventInstance.FormatDescription() != null)
+                            {
+                                if (eventInstance.FormatDescription().IndexOf(sid, 0, StringComparison.OrdinalIgnoreCase) != -1)
+                                {
+                                    message += "\n" + eventInstance.FormatDescription();
+                                    time = eventInstance.TimeCreated.ToString();
+                                    break;
+                                }
+
+                            }
+                        }
+                    }
+                    catch { }
+                    if (time == null)
+                    {
+                        time = DateTime.Now.ToString();
+                    }
+
+                    richTextBox2.Text = "Сведения для администраторов:" +
+                        "\nИмя пользователя: "+userName + 
+                        "\nsid: " + sid +
+                        "\nСообщение: " + message +
+                        "\nИмя компьютера: " + Environment.MachineName.ToString() +
+                        "\nВремя:" + time;
+                    send_message(userName, sid, message, time);
+                }
+                else
+                {
+                    this.Close();
+                }
+
+
+            }
+        }  
+    }
+}
